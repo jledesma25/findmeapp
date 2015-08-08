59c012e119889d9f0efd8b5234e1c7907e9f221b
Kenji Oki <devkenoki@gmail.com>
2015-08-08 02:19:43 -0500

    Se agregó Clase que se sobreescribirá para tener un PreferenceActivity con Actionbar

---
+++ app/src/main/java/com/imast/findingme/supportpreferenceactivity/AppCompatPreferenceActivity.java
@@ -0,0 +1,14 @@
+package com.imast.findingme.supportpreferenceactivity;
+
+import android.preference.PreferenceActivity;
+
+import static com.imast.findingme.util.LogUtils.makeLogTag;
+
+/**
+ * Created by aoki on 03/08/2015.
+ */
+public class AppCompatPreferenceActivity extends PreferenceActivity {
+
+
+
+}
