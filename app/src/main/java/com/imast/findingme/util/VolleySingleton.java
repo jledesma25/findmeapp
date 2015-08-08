f10fea0bb44a7cb3b97cf23d4a93608a7074576a
LuisMiguel Cornejo <lmcornejov@gmail.com>
2015-08-08 06:21:56 +0000

    clase VolleySingleton

---
+++ app/src/main/java/com/imast/findingme/util/VolleySingleton.java
@@ -0,0 +1,66 @@
+package com.imast.findingme.util;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by aoki on 03/08/2015.
+ */
+public class VolleySingleton {
+
+    // Atributos
+    private static VolleySingleton singleton;
+    private RequestQueue requestQueue;
+    private ImageLoader imageLoader;
+    private static Context context;
+
+    public VolleySingleton(Context context) {
+        VolleySingleton.context = context;
+        requestQueue = getRequestQueue();
+
+        imageLoader = new ImageLoader(requestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+    }
+
+    public static synchronized VolleySingleton getInstance(Context context) {
+        if (singleton == null) {
+            singleton = new VolleySingleton(context);
+        }
+        return singleton;
+    }
+
+    public ImageLoader getImageLoader() {
+        return imageLoader;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public  void addToRequestQueue(Request req) {
+        getRequestQueue().add(req);
+    }
+
+}
